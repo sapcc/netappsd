5e16a3f340c271f54c1b37aa7ec431c4d311317e
Chuan Miao <chuan137@gmail.com>
2019-07-18 10:37:40 +0200

    Write data to configmap

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,33 @@
 package main
 
 import (
-	"log"
+	"os"
+
+	klog "github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/log/level"
 
 	"netappsd"
 )
 
+var (
+	logger klog.Logger
+)
+
+func init() {
+	logger = klog.NewLogfmtLogger(klog.NewSyncWriter(os.Stdout))
+}
+
 func main() {
-	cm, err := netappsd.NewConfigMapOutofCluster("test-cm", "kube-monitoring", nil)
+	cm, err := netappsd.NewConfigMapOutofCluster("test-cm", "netapp2", logger)
+	logError(err)
+	err = cm.Write("test-field", `a: test-data
+b: fun to do
+`)
+	logError(err)
+}
+
+func logError(err error) {
 	if err != nil {
-		log.Println(err)
+		level.Error(logger).Log("msg", err)
 	}
-	log.Println(cm)
 }
