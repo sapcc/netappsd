b30ac6456d7bd482e06519f5d0857b54f6da8856
chuan137 <chuan137@gmail.com>
2023-11-23 14:24:17 +0100

    split master and worker cmd

---
+++ cmd/netappsd-worker/main.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"text/template"
+	"time"
+
+	"github.com/sapcc/netappsd/internal/pkg/netbox"
+	"github.com/spf13/cobra"
+)
+
+var (
+	debug            bool
+	log              *slog.Logger
+	logLvl           *slog.LevelVar
+	masterUrl        string
+	templateFilePath string
+	outputFilePath   string
+)
+
+var cmd = &cobra.Command{
+	Use:  "worker",
+	Long: `A simple network application service daemon worker`,
+	PreRun: func(cmd *cobra.Command, _ []string) {
+		if debug {
+			logLvl.Set(slog.LevelDebug)
+			log.Debug("debug logging enabled")
+		}
+	},
+	RunE: func(_ *cobra.Command, _ []string) error {
+		log.Info("Starting netappsd worker", "master-url", masterUrl, "template-file", templateFilePath, "output-file", outputFilePath)
+
+		tpl, err := template.ParseGlob(templateFilePath)
+		if err != nil {
+			return err
+		}
+
+		// TODO: add graceful shutdown
+		startWorker(tpl)
+		return nil
+	},
+}
+
+func main() {
+	cmd.Flags().BoolVarP(&debug, "debug", "d", false, "Enable debug logging")
+	cmd.Flags().StringVarP(&masterUrl, "master-url", "m", "http://localhost:8080", "The url of the netappsd-master")
+	cmd.Flags().StringVarP(&outputFilePath, "output-file", "o", "_output/arm64/harvest.yaml", "The path to the output file")
+	cmd.Flags().StringVarP(&templateFilePath, "template-file", "t", "deployments/harvest.yaml.tpl", "The path to the template file")
+	// cmd.Flags().StringVarP(&masterUrl, "master-url", "m", "http://netappsd-master.netapp-exporters.svc:8080", "The url of the netappsd-master")
+	// cmd.Flags().DurationVarP(&sleepTime, "sleep-time", "s", 10*time.Second, "The time to sleep between requests")
+
+	if err := cmd.Execute(); err != nil {
+		fmt.Println(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	logLvl = new(slog.LevelVar)
+	log = slog.New(slog.NewTextHandler(os.Stderr, &slog.HandlerOptions{AddSource: false, Level: logLvl}))
+}
+
+func startWorker(tpl *template.Template) {
+	ch := make(chan struct{})
+
+	go func() {
+		for ; true; <-time.After(1 * time.Second) {
+
+			resp, err := http.Get(masterUrl + "/next/filer.json")
+			if err != nil {
+				log.Error("Failed to get next filer", "error", err)
+				continue
+			}
+			if resp.StatusCode != 200 {
+				log.Error("Failed to get next filer", "status", resp.StatusCode)
+				continue
+			}
+
+			// print response body
+			respBody := &bytes.Buffer{}
+			_, err = respBody.ReadFrom(resp.Body)
+			if err != nil {
+				log.Error("Failed to read response body", "error", err)
+				continue
+			}
+			log.Debug("Response body", "body", respBody.String())
+
+			var b bytes.Buffer
+			var filer netbox.Filer
+			err = json.NewDecoder(respBody).Decode(&filer)
+			if err != nil {
+				log.Error("Failed to decode filer", "error", err)
+				continue
+			}
+			err = tpl.Execute(&b, filer)
+			if err != nil {
+				log.Error("Failed to execute template", "error", err)
+				continue
+			}
+
+			// write to file
+			f, err := os.Create(outputFilePath)
+			if err != nil {
+				log.Error("Failed to create file", "path", outputFilePath, "error", err)
+				continue
+			}
+			_, err = f.Write(b.Bytes())
+			if err != nil {
+				log.Error("Failed to write to file", "path", outputFilePath, "error", err)
+				continue
+			}
+			f.Close()
+			return
+
+			// body, err := ioutil.ReadAll(resp.Body)
+			// if err != nil {
+			// 	fmt.Println(err)
+			// }
+			// // close response body
+			// resp.Body.Close()
+			//
+			// // print response body
+			// fmt.Println(string(body))
+			// // if resp != nil {
+			// // 	log.Println(resp.StatusCode)
+			// // 	log.Println(resp.Request.GetBody())
+			// // 	ch <- struct{}{}
+			// // 	// return
+			// // }
+		}
+	}()
+
+	<-ch
+	for {
+		log.Info("Worker is running")
+		time.Sleep(10 * time.Second)
+	}
+}
---
+++ cmd/netappsd/netappsd.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+
+	"github.com/gorilla/mux"
+	"github.com/sapcc/netappsd/internal/netappsd"
+	"github.com/sapcc/netappsd/internal/pkg/netbox"
+)
+
+type NetAppSD struct {
+	*netappsd.NetAppSD
+}
+
+func NewNetAppSD() *NetAppSD {
+	netboxClient, err := netbox.NewClient(netboxURL, netboxToken)
+	if err != nil {
+		panic(err.Error())
+	}
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return &NetAppSD{
+		&netappsd.NetAppSD{
+			NetboxClient:        &netboxClient,
+			KubernetesClientset: clientset,
+			Region:              "qa-de-1",
+			ServiceType:         "manila",
+			Namespace:           "netapp-exporters",
+			AppLabel:            "app=netapp-harvest",
+		},
+	}
+}
+
+// AddTo implements the go-bits/httpapi.API interface. It registers the handler
+// for the /next/filer.json endpoint, which returns the next filer to be
+// harvested.
+func (n *NetAppSD) AddTo(r *mux.Router) {
+	r.Methods("GET").Path("/{replicaset}/next/filer.json").
+		HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			replicaset := mux.Vars(r)["replicaset"]
+			filer, err := n.Next(replicaset)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			json.NewEncoder(w).Encode(filer)
+		})
+}
