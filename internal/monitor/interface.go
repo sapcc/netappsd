2659fb1ef5ff19912ae63ba87f0b8c9227694b44
chuan137 <chuan137@gmail.com>
2024-02-17 15:22:56 +0100

    remove old files

--- internal/monitor/interface.go
+++
@@ -1,6 +0,0 @@
-package monitor
-
-type Discoverer interface {
-	// Discover returns discovered data, warnings and error
-	Discover(region, query string) (data map[string]interface{}, warns []error, e error)
-}
