c2dece58e49cb4bb1295e2ef4cb5d637a7e3c670
chuan137 <chuan137@gmail.com>
2024-03-14 15:15:21 +0100

    master: add metrics `netappsd_probe_filer_errors`

--- internal/netappsd/metrics.go
+++ internal/netappsd/metrics.go
@@ -14,9 +14,15 @@ var (
 		Name: "netappsd_worker_replicas",
 		Help: "Number of worker replicas.",
 	}, []string{})
+
+	probeFilerErrors = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "netappsd_probe_filer_errors",
+		Help: "Number of errors encountered while probing filer.",
+	}, []string{"filer", "filer_host"})
 )
 
 func init() {
 	prometheus.MustRegister(discoveredFiler)
 	prometheus.MustRegister(workerReplicas)
+	prometheus.MustRegister(probeFilerErrors)
 }
